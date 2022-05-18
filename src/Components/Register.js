f0697b5ebf547beb7a3483a18dac3ac3c2f37b6e
Carlos Marten <martencarlos@gmail.com>
2022-05-18 08:39:19 +0200

    working validation

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -24,58 +24,48 @@ export default function Register(props){
     
     function handleChange(event) {
         
-        if(formErrors.name){
-            const currentErrors = validateForm()
-            if(!currentErrors.name){
-                setFormErrors(currentErrors)
-            }
-        }
-
         const {name, value} = event.target
         setFormData(prevFormData => ({
             ...prevFormData,
             [name]: value
         }))
+        
     }
+
     function validateForm(){
-        const errors= {
-            name: "", 
-            username: "",
-            email:"",
-            password: "",
-            password2: ""
-        }
+        
+        const errors= {}
         
         if(!formData.name){
-            errors.name = "name is required"
+            errors.name = "Full Name is required"
         }
         if(!formData.username){
-            errors.username = "username is required"
+            errors.username = "Username is required"
         }else if(formData.username.indexOf(' ') >= 0){
             errors.username = "no white space permitted"
         }
         if(!formData.email){
-            errors.email = "email is required"
+            errors.email = "Email is required"
         }else if(!String(formData.email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-            errors.email = "email is invalid"
+            errors.email = "Email is invalid"
         }
         if(!formData.password){
-            errors.password = "password is required"
-        }else if(formData.password != formData.password2){
-            errors.password2 = "passwords do not match"
+            errors.password = "Password is required"
+        }else if(formData.password !== formData.password2){
+            errors.password2 = "Passwords do not match"
         }
+        
         return errors
     }
 
-    function validate(){
-        
-        const currentErrors = validateForm()
-        setFormErrors(currentErrors)
-        console.log(currentErrors)
-        // if(Object.keys(currentErrors).length===0){
-        //     console.log(formData)
-        // }
+    function validate(event){
         //event.preventDefault();
+        const currentErrors = validateForm(event.target.name)
+        setFormErrors(currentErrors)
+        console.log(Object.keys(currentErrors).length)
+        if(Object.keys(currentErrors).length===0){
+             console.log("submitting form")
+        }
     }
 
     return (
@@ -87,78 +77,96 @@ export default function Register(props){
                         <span  >
                             <i className="bi-people-fill" role="img" aria-label="name"></i>
                         </span>
-                        <input
-                            name="name"
-                            type="name"
-                            placeholder="name"
-                            autoComplete="section-blue name"
-                            required="required"
-                            value={formData.name}
-                            onChange={handleChange}
-                            onBlur = {validate}
-                        />
+                        <div className="register-form-input-row-inputanderror">
+                            <input
+                                name="name"
+                                type="name"
+                                placeholder="Full Name"
+                                autoComplete="section-blue name"
+                                required="required"
+                                value={formData.name}
+                                onChange={handleChange}
+                               
+                            />
+                            {formErrors.name && <label className="error">{formErrors.name}</label>}
+                        </div>
                     </div>
-                    {formErrors.name && <label>{formErrors.name}</label>}
+                    
 
                     <div className="register-form-input-row">
                         <span>
                             <i className="bi-person-badge-fill" role="img" aria-label="name"></i>
                         </span>
-                        <input
-                            name="username"
-                            type="nickname"
-                            placeholder="username"
-                            autoComplete="section-blue nickname"
-                            required="required"
-                            value={formData.username}
-                            onChange={handleChange}
-                        />
+                        <div className="register-form-input-row-inputanderror">
+                            <input
+                                name="username"
+                                type="nickname"
+                                placeholder="username"
+                                autoComplete="section-blue nickname"
+                                required="required"
+                                value={formData.username}
+                                onChange={handleChange}
+                                
+                            />
+                            {formErrors.username && <label className="error">{formErrors.username}</label>}
+                        </div>
                     </div>
 
                     <div className="register-form-input-row">
                         <span>
                             <i className="bi-envelope-fill" role="img" aria-label="name"></i>
                         </span>
-                        <input
-                            name="email"
-                            type="email"
-                            placeholder="email"
-                            autoComplete="section-blue email"
-                            required="required"
-                            value={formData.email}
-                            onChange={handleChange}
-                        />
+                        <div className="register-form-input-row-inputanderror">
+                            <input
+                                name="email"
+                                type="email"
+                                placeholder="email"
+                                autoComplete="section-blue email"
+                                required="required"
+                                value={formData.email}
+                                onChange={handleChange}
+                                
+                            />
+                            {formErrors.email && <label className="error">{formErrors.email}</label>}
+                        </div>
                     </div>
                     <div className="register-form-input-row">
                         <span>
                             <i className="bi-key-fill" role="img" aria-label="name"></i>
                         </span>
-                        <input
-                            name="password"
-                            type="password"
-                            placeholder="password"
-                            autoComplete="section-red new-password"
-                            required="required"
-                            value={formData.password}
-                            onChange={handleChange}
-                        />
+                        <div className="register-form-input-row-inputanderror">
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="password"
+                                autoComplete="section-red new-password"
+                                required="required"
+                                value={formData.password}
+                                onChange={handleChange}
+                                
+                            />
+                            {formErrors.password && <label className="error">{formErrors.password}</label>}
+                        </div>
                     </div>
                     <div className="register-form-input-row">
                         <span>
                             <i className="bi-key-fill" role="img" aria-label="name"></i>
                         </span>
-                    <input
-                        name="password2"
-                        type="password"
-                        placeholder="re-enter password"
-                        autoComplete="section-red new-password"
-                        required="required"
-                        value={formData.password2}
-                        onChange={handleChange}
-                        />
+                        <div className="register-form-input-row-inputanderror">
+                            <input
+                                name="password2"
+                                type="password"
+                                placeholder="re-enter password"
+                                autoComplete="section-red new-password"
+                                required="required"
+                                value={formData.password2}
+                                onChange={handleChange}
+                                
+                            />
+                            {formErrors.password2 && <label className="error">{formErrors.password2}</label>}
+                        </div>
                     </div>
                     <button className="register" type="button" onClick={validate}>Register</button>
-                    
                 </div>
             </form>
         </div>
