04d652eccba5329310fbe30dd87126d8d38e026f
Carlos Marten <martencarlos@gmail.com>
2022-06-01 15:30:02 +0200

    Navbar and footer now mobile friendly

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,18 +3,26 @@ import "../css/footer.css";
 
 export default function Footer(props){
     console.log("Rendering Footer")
+    
+    const year= new Date().getFullYear()
+
     return (
         <footer className={props.darkMode ? "dark" : ""}>
-            
-            <section className="ft-legal">
-                <ul className="ft-legal-list">
-                <li><a href="/">Terms &amp; Conditions</a></li>
-                <li><a href="/">Privacy Policy</a></li>
-                <li>&copy; 2022 Copyright Webframe Inc.</li>
-                </ul>
-            </section>
-        </footer>
+            <div className="footer-brand">
+                © {year} Webframe Inc
+            </div>
 
-        
+            <ul className="footer-links">
+                <li className="nav-item">
+                    <a href="#" className="">Features</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="">Blog</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="">About</a>
+                </li>
+            </ul>
+        </footer>
     )
 }
