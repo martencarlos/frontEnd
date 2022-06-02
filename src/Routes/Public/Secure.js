0e3f1158a0c3e6a6601b3f3a2af73ce7d761ec10
Carlos Marten <martencarlos@gmail.com>
2022-06-02 10:44:27 +0200

    making sure

--- src/Routes/Public/Secure.js
+++ src/Routes/Public/Secure.js
@@ -5,6 +5,7 @@ export default function Secure(){
     
     function returnId(){
         console.log(process.env.REACT_APP_SSL_KEY)
+        console.log("making sure")
         return (process.env.REACT_APP_SSL_KEY)
     }
    
