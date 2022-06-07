334585045299b8c23df3a91d362e30d8b2962c6b
Carlos Marten <martencarlos@gmail.com>
2022-06-07 11:20:56 +0200

    image upload and upload progress ui fixes

---
+++ src/Util/ImageProcessing.js
@@ -0,0 +1,32 @@
+import Resizer from "react-image-file-resizer";
+
+//Image processing functions
+
+export const resizeFile = async (file)  =>
+    new Promise((resolve) => {
+        Resizer.imageFileResizer(
+        file,
+        80,
+        80,
+        "JPEG",
+        100,
+        0,
+        (uri) => {
+            resolve(uri);
+        },
+        "base64"
+        );
+    });
+
+
+export const dataURIToBlob =  (dataURI) => {
+    const splitDataURI = dataURI.split(",");
+    const byteString =
+        splitDataURI[0].indexOf("base64") >= 0
+        ? atob(splitDataURI[1])
+        : decodeURI(splitDataURI[1]);
+    const mimeString = splitDataURI[0].split(":")[1].split(";")[0];
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i);
+    return new Blob([ia], { type: mimeString });
+};
