6b9b5d146683539545e7140cc875ed2eafc43966
Carlos Marten <martencarlos@gmail.com>
2022-06-08 14:47:39 +0200

    ongoing deleting card feature

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,21 +6,24 @@ export default function Card(props){
     console.log("Rendering Card")
     
     return (
-        <div  className="card">
+        <div id={props.item._id}  className="card">
+            <span onClick= {props.deleteCard} className="card--delete">
+                <i className="bi-x-lg" role="img"></i>
+            </span>
             <img className="card--info-img"
                     src={props.item.image}
                     alt="cover"
-                />
+            />
             <div className="card--info">
-                <span>
+                <span className="card--stats">
                     <i className="bi-star" role="img"></i>
                         {props.item.stats.likes}
                 </span>
-                <span>
+                <span className="card--stats">
                         <i className="bi-eye"></i>
                         {props.item.stats.views}
                 </span>
-                <span>
+                <span className="card--stats">
                         <i className="bi-box-arrow-in-down"></i>
                         {props.item.stats.downloads}
                 </span>
