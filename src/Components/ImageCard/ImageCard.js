39ea7c6b6459308e694c3e6cd3c91e274f33d670
Carlos Marten <martencarlos@gmail.com>
2022-09-11 12:07:13 +0200

    Added feature row + refactoring of Card

---
+++ src/Components/ImageCard/ImageCard.js
@@ -0,0 +1,43 @@
+
+import "./imageCard.css";
+
+
+export default function imageCard(props){
+    console.log("Rendering imageCard")
+    
+    return (
+        <div id={props.item._id}  className="imageCard">
+            {props.showDeleteButton && <span onClick= {props.deleteimageCard} className="imageCard--delete">
+                <i className="bi-x-lg" role="img"></i>
+            </span>}
+            <img  className="imageCard--info-img"
+                    src={props.item.image}
+                    alt="cover"
+            />
+            <div className="imageCard--info">
+                <span className="imageCard--stats">
+                    <i className="bi-star" role="img"></i>
+                        {props.item.stats.likes}
+                </span>
+                <span className="imageCard--stats">
+                        <i className="bi-eye"></i>
+                        {props.item.stats.views}
+                </span>
+                <span className="imageCard--stats">
+                        <i className="bi-box-arrow-in-down"></i>
+                        {props.item.stats.downloads}
+                </span>
+            </div>
+            <div className={`imageCard--profile ${props.darkMode ? "dark": ""}`}>
+                <img  className="imageCard--profile--img"
+                    src={props.item.author.pic}
+                    alt="profile pic"
+                />
+                <div className="imageCard--profile--text">
+                    <h2 className="imageCard--profile--text--h2">{props.item.title}</h2>
+                    <p className="imageCard--profile--author"> <b>{props.item.author.firstName + " " + props.item.author.lastName}</b></p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Routes/Public/Features/Features.js
+++ src/Routes/Public/Features/Features.js
@@ -2,18 +2,42 @@
 import "./features.css";
 
 import SectionHeader from "../../../Components/SectionHeader/SectionHeader";
+import Card from "../../../Components/Card/Card";
 
-import { featuresSection } from "../../../data/features_data"
+import { featuresSection, featureCard1, featureCard2, featureCard3 } from "../../../data/features_data"
 
 export default function Features(props){
     console.log("Rendering Features")
     return (
         <div className= {`features ${props.darkMode ? "dark": ""}`}>
+           
            <SectionHeader 
                 darkMode = {props.darkMode}
                 title = {featuresSection.title}
                 text = {featuresSection.text}
             />
+
+            <div className="features-row">
+                <Card 
+                    darkMode = {props.darkMode}
+                    icon = {featureCard1.icon}
+                    title = {featureCard1.title}
+                    text = {featureCard1.text}
+                />
+                <Card 
+                    darkMode = {props.darkMode}
+                    icon = {featureCard2.icon}
+                    title = {featureCard2.title}
+                    text = {featureCard2.text}
+                />
+                <Card 
+                    darkMode = {props.darkMode}
+                    icon = {featureCard3.icon}
+                    title = {featureCard3.title}
+                    text = {featureCard3.text}
+                />
+            </div>
+
         </div>
     )
 }
