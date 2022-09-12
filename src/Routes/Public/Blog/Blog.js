fc3deeab71e1f0945d55d1409bfd40c4aa7791d6
Carlos Marten <martencarlos@gmail.com>
2022-09-12 11:16:53 +0200

    Updated blog structure

--- src/Routes/Public/Blog/Blog.js
+++ src/Routes/Public/Blog/Blog.js
@@ -1,23 +1,21 @@
 
 import "./blog.css";
-import Summary from "../../../Components/ArticleSummary";
-import Article from "../../../Components/Article";
+import Summary from "../../../Components/ArticleSummary/ArticleSummary";
+import Article from "../../../Components/Article/Article";
 
-import { useState,useEffect,useRef} from "react"
+import { useState,useEffect, useRef} from "react"
 
 import * as rssParser from 'react-native-parser-rss';
- 
-
 
 export default function Blog(props){
     console.log("Rendering Blog")
-    const lastArticle = useRef({});
+    
     const [posts, setPosts] = useState([])
     const [mainArticle, setMainArticle] = useState({})
 
     //Load articles from Medium
     useEffect(() => {
-
+        console.log("Blog useEffect - load articles from medium")
         async function getData() {
             await fetch(process.env.REACT_APP_SERVER+'/medium',{
                 method: 'GET',
@@ -31,11 +29,9 @@ export default function Blog(props){
               .then((feed) => {
                   setPosts(feed.items);
                   setMainArticle(feed.items[0])
-                  //lastArticle.current=feed.items[0]
                 });
         }
         getData()
-
     }, [])
 
     useEffect(() => {
@@ -44,27 +40,14 @@ export default function Blog(props){
         }
     }, [posts])
     
+    //open article by using event.currentTarget id
     function openArticle(e){
         console.log(posts.find(x => x.id === e.currentTarget.id))
         setMainArticle(posts.find(x => x.id === e.currentTarget.id))
-        //posts.find(x => x.id === e.currentTarget.id)
     }
 
     return (
         <div className={`blog ${props.darkMode ? "dark": ""}`}>
-            
-            <div  className="blog-posts" >
-                <br></br> 
-                {posts.map((post, i) => (
-                    <Summary
-                        key = {i}
-                        darkMode = {props.darkmode}
-                        item = {post}
-                        openArticle = {openArticle}
-                    />
-                
-                ))}
-            </div>
             
             {mainArticle && 
             <div  className="blog-mainArticle">
@@ -72,8 +55,27 @@ export default function Blog(props){
                         darkMode = {props.darkmode}
                         item = {mainArticle}
                     />
+            </div>}
+            
+            <div  className="blog-posts" >
+                <br></br>
+                <div className="blog-posts-title"> Latest updates</div>
+                <br></br>
+                {posts.map((post, i) => (
+                    <div>
+                        <Summary
+                        key = {i}
+                        darkMode = {props.darkmode}
+                        item = {post}
+                        openArticle = {openArticle}
+                        />
+                        <div className="separator"></div>
+                    </div>
+                ))}
+
+                
             </div>
-            }
-            </div>
+
+        </div>
     )
 }
