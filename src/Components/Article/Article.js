c1a4ec85c4a4899bded6f15d383e3f03e3829cb7
Carlos Marten <martencarlos@gmail.com>
2022-09-12 16:08:39 +0200

    article fix

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -12,7 +12,6 @@ export default function Article(props){
             <div className="article">
                 <div  className="title">  {props.item.title}  </div>
                 <div  className="published"> {props.item.published.substring(0,16)}</div> 
-   
                 <div className={`post ${props.darkMode ? "dark": ""}`} dangerouslySetInnerHTML={{__html: props.item.content.substring(0, props.item.content.length - 133)}} />
             </div>
     )
