0f8e8aae1b97925ee050ca6ae7727317a62ee2a1
Carlos Marten <martencarlos@gmail.com>
2022-09-13 21:01:54 +0200

    final design updates

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,9 @@
 
 import "./card.css";
 
+
+import Typography from '@mui/material/Typography';
+
 export default function Card(props){
 
     console.log("Rendering Card component")
@@ -8,14 +11,15 @@ export default function Card(props){
     return (
         <div className= {`card ${props.darkMode ? "dark": ""}`}>
             {/* {props.direction === "right" && <img src= {props.imgSrc} alt=""></img>} */}
-            {props.imgType === "icon" && <i className={props.icon}></i>}
+            {props.imgType === "icon" && <i className={'features-icon-color' && props.icon}></i>}
             {props.imgType === "image" && <img alt="" src={props.imgSrc}></img>}
             <div className="text-area">
-                <div className="title">{props.title}</div>
-                <div className="text">{props.text}</div>
+                <Typography variant="h5" className="title">{props.title}</Typography>
+                <Typography variant="body1" className="text">{props.text}</Typography>
             </div>
             
         </div>
     )
 }
+
 
--- src/data/main_data.js
+++ src/data/main_data.js
@@ -18,7 +18,7 @@ const infinityCards =
                 `A project based on a basic free React course where you can create and delete cards. 
 
                 The cards are responsive based on the size of the screen and dynamically loaded when scrolling down. Incredibly useful for every web application.`,
-        linkText: "Go to demo >"
+        linkText: "Go to demo  ➤"
     }
 ;
 
@@ -29,7 +29,7 @@ const blogIntegration =
                 `A blog is a basic component for any website where the team can share news with the world. But to create a blog requires a lot of time. 
 
                 Why not integrate with a great Blog editor like Medium?`,
-        linkText: "Go to demo >"
+        linkText: "Go to demo  ➤"
     }
 ;
 
