b03ef4757c77de36bee634852873da85fee101ab
Carlos Marten <martencarlos@gmail.com>
2022-09-15 20:01:29 +0200

    footer fix and minor color corrections on blog dark mode. also changed the color of the dark mode switch

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -30,8 +30,8 @@ export default function Article(props){
             <div>
                 <div className="article">
                     
-                    <Typography variant="h4" gutterBottom>{props.item.title} </Typography>
-                    <Typography variant="h5" gutterBottom className="published"> {props.item.published.substring(0,16)}</Typography> 
+                    <Typography className="article-title" variant="h4" gutterBottom>{props.item.title} </Typography>
+                    <Typography className="published" variant="h5" gutterBottom > {props.item.published.substring(0,16)}</Typography> 
                     <Typography variant="body1" gutterBottom className={`post ${props.darkMode ? "dark": ""}`} dangerouslySetInnerHTML={{__html: props.item.content.substring(0, props.item.content.length - 133)}} />
                     {progress>=15 && 
                         <Fab onClick={props.scrollToTop}
@@ -42,7 +42,7 @@ export default function Article(props){
                     } 
                 </div>
                 {progress>=2 && 
-                    <LinearProgress className="reading-progress" variant="determinate" value={progress}/>
+                    <LinearProgress color='secondary' className="reading-progress" variant="determinate" value={progress}/>
                 }
             </div>
     )
