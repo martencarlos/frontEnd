b94a2edcff653db5309e9e01347b4bd722745a88
Carlos Marten <martencarlos@gmail.com>
2022-09-15 19:12:46 +0200

    v.2.0 Major website redesign: fixed complete css, fixed and updated overall color theme.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import "./navbar.css";
+
 import {Link, NavLink} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import {getCookie} from "../../Util/Cookie";
@@ -6,6 +7,57 @@ import {useState, useEffect} from "react"
 
 import Button from '@mui/material/Button'
 import Tooltip from '@mui/material/Tooltip';
+
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+      margin: 1,
+      padding: 0,
+      transform: 'translateX(6px)',
+      '&.Mui-checked': {
+        color: '#fff',
+        transform: 'translateX(22px)',
+        '& .MuiSwitch-thumb:before': {
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            '#fff',
+          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        },
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+      width: 32,
+      height: 32,
+      '&:before': {
+        content: "''",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+      },
+    },
+    '& .MuiSwitch-track': {
+      opacity: 1,
+      backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      borderRadius: 20 / 2,
+    },
+  }));
 
 export default function Navbar(props){
     console.log("Rendering Navbar")
@@ -80,14 +132,21 @@ export default function Navbar(props){
                     <li><Button className= {`bt-nav-link ${props.darkMode ? "dark": ""}`}  variant="text"><NavLink className={({ isActive }) =>isActive ? "nav-link-active" : "nav-link"}  to="/blog">Blog</NavLink></Button></li>
                     <li><Button className= {`bt-nav-link ${props.darkMode ? "dark": ""}`}  variant="text"><NavLink className={({ isActive }) =>isActive ? "nav-link-active" : "nav-link"}  to="/about">About</NavLink></Button></li>
                 </ul>
-                {!props.login && <div className="sign-buttons">
-                    <Button variant="outlined" className= {`nav-button ${props.darkMode ? "dark": ""}`} type="button" onClick={() => navigate('/login')}>Login</Button>
-                    <Button variant="contained" className="nav-button" type="button" onClick={() => navigate('/register')}>Register</Button>
-                        <div className="toggler" >
+                {!props.login && 
+                    <div className="sign-buttons">
+                        <Button variant="outlined" className= {`nav-button ${props.darkMode ? "dark": ""}`} type="button" onClick={() => navigate('/login')}>Login</Button>
+                        <Button variant="contained" className="nav-button" type="button" onClick={() => navigate('/register')}>Register</Button>
+                        
+                        <FormControlLabel className="toggler"
+                            control={<MaterialUISwitch sx={{ m: 1 }} color='primary' onClick= {props.toggleDarkMode}/>}
+                            
+                        />
+                        {/* <div className="toggler" >
                             <div className="toggler--slider" onClick={props.toggleDarkMode} >
                                 <div className="toggler--slider--circle"></div>
                             </div>
-                        </div>
+                        </div> */}
+
                     </div>
                 }
 
@@ -101,11 +160,10 @@ export default function Navbar(props){
                             </Tooltip>
                         }
                         <Button variant="outlined" className="nav-button" type="button" onClick={() => navigate('/logout')}>Logout</Button>
-                        <div className="toggler" >
-                            <div className="toggler--slider" onClick={props.toggleDarkMode} >
-                                <div className="toggler--slider--circle"></div>
-                            </div>
-                        </div>
+                        
+                        <FormControlLabel className="toggler"
+                            control={<MaterialUISwitch sx={{ m: 1 }} color='primary' onClick= {props.toggleDarkMode}/>}
+                        />
                     </div>
                 }
 
@@ -125,11 +183,9 @@ export default function Navbar(props){
                     <li><Button className="bt-nav-link" variant="text"><NavLink className={({ isActive }) =>isActive ? "nav-link-active" : "nav-link"}  to="/about">About</NavLink></Button></li>
                 </div>
 
-                <div className="toggler" >
-                    <div className="toggler--slider" onClick={props.toggleDarkMode} >
-                        <div className="toggler--slider--circle"></div>
-                    </div>
-                </div>
+                <FormControlLabel className="toggler"
+                    control={<MaterialUISwitch sx={{ m: 1 }} color='primary' onClick= {props.toggleDarkMode}/>}
+                />
             </div>
         </div>
     )
