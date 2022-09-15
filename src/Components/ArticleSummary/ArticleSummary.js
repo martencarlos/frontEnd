1bf7d0be15dca1c2d383fd6c676c67a76695dc68
Carlos Marten <martencarlos@gmail.com>
2022-09-15 20:01:29 +0200

    footer fix and minor color corrections on blog dark mode. also changed the color of the dark mode switch

--- src/Components/ArticleSummary/ArticleSummary.js
+++ src/Components/ArticleSummary/ArticleSummary.js
@@ -2,6 +2,7 @@
 
 import "./articleSummary.css";
 
+import Typography from '@mui/material/Typography';
 
 export default function ArticleSummary(props){
     console.log("Rendering ArticleSummary")
@@ -18,10 +19,11 @@ export default function ArticleSummary(props){
             <img alt="img" src= {props.item.imageUrl} className="summaryImage"></img>
             
             <div className="textArea">
-                <div  className="title">  {props.item.title}  </div>
-                <div  className="published"> {props.item.published.substring(0,16)}</div> 
-                <div  className="summary"> {props.item.summary}</div>
+                <Typography className="title" variant="body1" >  {props.item.title} </Typography>
+                <Typography  className="published" > {props.item.published.substring(0,16)}</Typography> 
+                <Typography className="summary" variant="body2" > {props.item.summary}</Typography>
             </div>
         </div>
     )
 }
+
