8a9902c96f348375fa521ae8305be81b8843b152
Carlos Marten <martencarlos@gmail.com>
2022-09-25 11:34:05 +0200

    improved performance with cache-control and migrated from png to webp the last static images.

--- src/Routes/Public/NotFound/NotFound.js
+++ src/Routes/Public/NotFound/NotFound.js
@@ -5,33 +5,40 @@ import Typography from '@mui/material/Typography';
 import '@fontsource/roboto/900.css';
 import { useEffect, useState } from "react";
 
-
+import Skeleton from '@mui/material/Skeleton';
 
 
 export default function NotFound(props){
 
     console.log("Rendering 404")
 
-    
+
+    const [imgLoading, setImgLoading] = useState(true)
     const [img404Src, setimg404Src] = useState()
+    
     const doggySrc= "https://firebasestorage.googleapis.com/v0/b/webframebase.appspot.com/o/static%20images%2Fdoggy.gif?alt=media&token=b976fefc-ebc2-4837-9aa8-080624a7501d"
+    
     useEffect (() => {
         var random = Math.floor(Math.random() * 2);
         if(random===1)
-            setimg404Src("https://firebasestorage.googleapis.com/v0/b/webframebase.appspot.com/o/static%20images%2F404%20-%20dog.png?alt=media&token=a048cd6c-9760-4715-81f4-e482ccd76898");
+            setimg404Src("https://firebasestorage.googleapis.com/v0/b/webframebase.appspot.com/o/static%20images%2F404%20-%20dog.webp?alt=media&token=ea18b5c9-ad4e-4d77-b599-361ffe9c9294");
         else
-            setimg404Src("https://firebasestorage.googleapis.com/v0/b/webframebase.appspot.com/o/static%20images%2F404%20-%20cat.png?alt=media&token=5dfbe803-bdf8-40ea-83a1-99987a9a3e02");
-
+            setimg404Src("https://firebasestorage.googleapis.com/v0/b/webframebase.appspot.com/o/static%20images%2F404%20-%20cat.webp?alt=media&token=7ba21798-95e6-4e76-ac46-2c399eae065f");
+        
+        setImgLoading(false)
     },[])
 
+  
     return (
         <div className= {`notFound ${props.darkMode ? "dark": ""}`}>
-            
-            <img src= {img404Src} alt="not found"></img>
+            {imgLoading ?
+                <Skeleton sx={{ width:200, height: 250}} animation="wave" variant="rectangular" />
+            :
+                <img src= {img404Src}  alt="not found"></img>}
             <div className="notFound-textArea">
                 <Typography className="notFound-title" variant="h1" gutterBottom>404</Typography>
                 <Typography className="notFound-text" variant="h5" gutterBottom>Page Not Found</Typography>
-                <img src= {doggySrc} className="doggy" alt="not found"></img>
+                <img src= {doggySrc} className="doggy"  alt="not found"></img>
             </div>
         </div>
     )
