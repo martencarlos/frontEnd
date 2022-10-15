c57593746d021738a7bca6b5d4455279e9408edd
Carlos Marten <martencarlos@gmail.com>
2022-10-15 21:44:13 +0200

    Updated dashboard

--- src/Routes/Private/Home/Pages/Users/Users.js
+++ src/Routes/Private/Home/Pages/Users/Users.js
@@ -1,62 +1,105 @@
  
 import "./users.css";
 
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
+import axios from "axios";
+
 import Typography from '@mui/material/Typography';
 import { DataGrid } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 
 
 export default function Users(props){
 
     console.log("Rendering Users")
 
+    const [users, setUsers] = useState()
+    const selectedRows = useRef();
+    // const [loading, setLoading] = useState(true)
+    
+
+    //Get all users from the database
+    useEffect(() => {
+        console.log("useEffect - get users")
+        const fetchUsers = async () => {
+            await axios.get(`${process.env.REACT_APP_SERVER}/users`)
+              .then(function (res) {
+                    
+                    //change the '_id' key for 'id' to be able to display in data grid. + reformat dates
+                    res.data = res.data.map(function(obj) {
+                        
+                        obj.createDate = new Date(Date.parse(obj.createDate)).toLocaleDateString();
+                        obj.lastUpdate = new Date(Date.parse(obj.lastUpdate)).toLocaleDateString();
+                        obj['id'] = obj['_id']; // Assign new key
+                        delete obj['_id']; // Delete old key
+                        return obj;
+                    });
+                    
+                    setUsers(res.data);
+                //   setLoading(false)
+  
+              })
+              .catch(function (error) {
+                  // handle error
+                  console.log(error);
+                //   setLoading(false)
+              })
+              .finally(function (response) {});
+          };
+        
+          if (true) fetchUsers();
+        
+    }, [])
+
+   
+
     const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 90,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
+        { field: 'id', headerName: 'ID', width: 220 },
+        { field: 'avatar', headerName: 'Avatar', width: 60, renderCell: (params)=>{
+            return (
+                <img className="datagrid-mini-avatar" src={params.row.profilePic} alt={"avatar"}></img>
+            )
+        } },
+        { field: 'name', headerName: 'Name', width: 130 },
+        { field: 'username', headerName: 'Username', width: 130 },
+        { field: 'email', headerName: 'Email', width: 200 },
+        { field: 'createDate', headerName: 'Create Date', width: 130 },
+        { field: 'lastUpdate', headerName: 'Last Update', width: 130 },
+        { field: 'actions', headerName: 'Actions', width: 80, renderCell: (params)=>{
+            return (
+                <div className="datagrid-actions">
+                    <EditIcon/>
+                    <DeleteIcon/>
+                </div>
+            )
+        } }
       ];
       
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
       
       
     return (
         <div className="users">
+            <Typography className="page-title" variant="h4"  gutterBottom> All Users </Typography>
             <div className="users-pannel">
-                <div style={{ height: 400, width: '100%' }}>
+                {users && 
+                <div style={{ height: 480, width: '100%' }}>
                     <DataGrid
-                    rows={rows}
+                    rows={users}
                     columns={columns}
-                    pageSize={9}
-                    rowsPerPageOptions={[9]}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    density={"compact"}
                     checkboxSelection
-                />
+                    onSelectionModelChange={(ids) => {
+                        const selectedIDs = new Set(ids);
+                        selectedRows.current = users.filter((user) =>{return selectedIDs.has(user.id.toString())}
+                        );
+                      }}
+                    />
+                </div>}
             </div>
-        </div>
       </div>
                 
     )
