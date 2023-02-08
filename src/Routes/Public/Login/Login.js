ee7e3caa66e083b1d7b95b02b2be9ac30ce557e1
Carlos Marten <martencarlos@gmail.com>
2023-02-08 19:56:04 +0100

    debug

--- src/Routes/Public/Login/Login.js
+++ src/Routes/Public/Login/Login.js
@@ -80,8 +80,10 @@ export default function Login(props){
         const config = {
             url: process.env.REACT_APP_SERVER+'/login',
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': process.env.SERVER,
             },
             data: JSON.stringify(user),
             withCredentials: true, // Now this is was the missing piece in the client side 
