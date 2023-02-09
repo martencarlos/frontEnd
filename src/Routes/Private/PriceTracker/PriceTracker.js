0faf3b38d3d17f9488cca418bb2ae977816e9304
Carlos Marten <martencarlos@gmail.com>
2023-02-09 21:24:19 +0100

    minor pricetracker ui update

--- src/Routes/Private/PriceTracker/PriceTracker.js
+++ src/Routes/Private/PriceTracker/PriceTracker.js
@@ -357,10 +357,10 @@ export default function Pricetracker(props){
                                             margin={{top: 0,right: 6,left: -12,bottom: 0,}}
                                             strokeWidth={2}
                                             >
-                                            <XAxis   padding={{ left: 30, right: 30 }} stroke="#f17e5b" dataKey="date" />
-                                            <YAxis type="number" domain={['dataMin-2', 'dataMax+2']} />
+                                            <XAxis  strokeWidth={1} padding={{ left: 30, right: 30 }} stroke="#f17e5b" dataKey="date" />
+                                            <YAxis strokeWidth={1} type="number" stroke="#f17e5b" domain={['dataMin-2', 'dataMax+2']} />
                                             <Tooltip />
-                                            <Line strokeWidth={2} type="monotone" dataKey="price" stroke="#f17e5b" activeDot={{ r: 8 }} />
+                                            <Line strokeWidth={2} type="monotone" dataKey="price" stroke="#f17e5b" activeDot={{ r: 6 }} />
                                         </LineChart>
                                     </ResponsiveContainer>
                                 </div>}
