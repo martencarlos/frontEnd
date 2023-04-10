e1e36bd27b91a302d058009a7415001970e97464
Carlos Marten <martencarlos@gmail.com>
2023-04-10 11:32:52 +0200

    landpage text update

--- src/data/main_data.js
+++ src/data/main_data.js
@@ -7,7 +7,7 @@ const landingHero =
         text: 
                 `Create a full-featured Website Application fast and effortlessly. Take a look at some website projects created with Webframe in the /projects tab.
 
-                Wordpress, Webflow, Shopify, Squarespace or Wix are great for standard static website creation. But if you are looking to create your own professional Website Application with us, check out Webframe.`
+                Wordpress, Webflow, Shopify, Squarespace or Wix are great for standard static website creation. But if you are looking to create your own professional Website Application without limitations and with the help of experts, try Webframe.`
     }
 ;
 
