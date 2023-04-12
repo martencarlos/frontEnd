97f2376690526637577f43dc29d81efa6ca6d9ca
Carlos Marten <martencarlos@gmail.com>
2023-04-12 12:41:32 +0200

    section header size fix

--- src/Components/SectionHeader/SectionHeader.js
+++ src/Components/SectionHeader/SectionHeader.js
@@ -7,7 +7,7 @@ export default function HeroRight(props){
     console.log("Rendering section Header component")
     return (
         <div className= {`sectionHeader ${props.darkMode ? "dark": ""}`}>
-           <Typography variant="h5" className="title">{props.title}</Typography>
+           <Typography variant="h3" className="title">{props.title}</Typography>
            <Typography variant="body1" className="text">{props.text}</Typography>
         </div>
     )
