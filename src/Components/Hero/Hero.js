2de86df8ca6be4a50431b37b1371ef58ed2cb155
Carlos Marten <martencarlos@gmail.com>
2023-06-01 14:09:40 +0200

    Landing page - get in touch update

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,18 +3,82 @@ import "./hero.css";
 
 // import { useEffect,useRef} from "react"
 import Typography from '@mui/material/Typography';
+import {useState,Fragment,forwardRef} from "react";
 // import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import ForwardIcon from '@mui/icons-material/Forward';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import axios from "axios";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
 
 import Typed from 'react-typed';
+import { grey, red } from "@mui/material/colors";
 
 export default function Hero(props){
     console.log("Rendering Hero component")
 
+    const Alert = forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+      });
+    
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
+    const snackbarHandleClick = () => {
+        setSnackbarOpen(true);
+    };
+  
+    const snackbarHandleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+      setSnackbarOpen(false);
+    };
 
+    const [openContactUs, setOpenContactUs] = useState(false);
+    const handleOpenContactUs = () => setOpenContactUs(true);
+    const handleCloseContactUs = () => setOpenContactUs(false);
+    const [formData, setFormData] = useState({
+        name:"",
+        email: "",
+        message: "",
+    }
+)
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: value
+        }))
+    }
+
+    function sendEmail() {
+        console.log("Sending email")
+
+        const config = {
+            url: process.env.REACT_APP_SERVER+'/send-email',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify({name:formData.name ,email:formData.email, message:formData.message}),
+            
+        };
+
+        axios(config).then(function (response) {
+            console.log(response.data)
+            snackbarHandleClick()
+        }).catch(function (error) {
+            console.log(error);
+        });
+        handleCloseContactUs()
+    }
 
     return (
         <div className= {`hero ${props.darkMode ? "dark": ""}`}>
@@ -56,9 +120,62 @@ export default function Hero(props){
                 }
                 {props.button &&
                 <div className="hero-button">
-                    <Button href="mailto:martencarlos@gmail.com?subject=Webframe - contact" style={{backgroundColor:"#59887bfc", color: "white"}}  variant="contained" endIcon={<SendIcon />}>
-                        Get in touch
+                    <Button onClick={handleOpenContactUs} style={{ width:"250px",backgroundColor:"#59887bfc", color: "white"}}  variant="contained" endIcon={<SendIcon />}>
+                        Get in touch  
                     </Button>
+                    <Modal
+                        open={openContactUs}
+                        onClose={handleCloseContactUs}
+                    >
+                        {/* <div className={`contact-modal ${props.darkMode ? "dark": ""}`}> */}
+                        <div className="contact-modal">
+                            <div className="close-modal" style={{right: "20px", position:"absolute"}}>
+                            <IconButton onClick={handleCloseContactUs}>
+                                <CloseIcon />
+                            </IconButton>
+                            </div>
+                            <div className="row">
+                                <Typography variant="h4" color={grey}>Get in touch</Typography>
+                            </div>
+                            <div className="row">
+                                <div className="contact-modal-image">
+                                    <img src="https://firebasestorage.googleapis.com/v0/b/webframebase.appspot.com/o/static%20images%2FLanding%20Page%20%20-%20contact%20us.webp?alt=media&token=63463283-7be5-49f7-8ecd-e85e08728ea8&_gl=1*1pijch4*_ga*MTY2MDkzMTMyMC4xNjgxNzQxNTU3*_ga_CW55HF8NVT*MTY4NTYxMzY4OC4zLjEuMTY4NTYxMzc0NS4wLjAuMA" alt=""></img>
+                                </div>
+
+                                <form className="contact-modal-form">
+                                    
+                                    <TextField required onChange={handleChange} style={{width:"100%"}} size="small" name="name" label="Name" variant="outlined" />
+                                    <TextField required onChange={handleChange} style={{width:"100%"}} size="small" name="email" label="Email" variant="outlined" />
+                                    <TextField
+                                        required
+                                        onChange={handleChange}
+                                        name="message"
+                                        style={{width:"100%"}}
+                                        placeholder="Please enter your message here *"
+                                        multiline
+                                        rows={3}
+                                        maxRows={10}
+                                    />
+                                    <Button onClick={sendEmail} style={{width:"100%", backgroundColor:"#59887bfc", color: "white"}}  variant="contained" endIcon={<SendIcon />}>
+                                        Send email
+                                    </Button>
+                                    
+                                </form>
+                            </div>
+                        </div>
+                    </Modal>
+                    <Snackbar
+                        open={snackbarOpen}
+                        type="success"
+                        autoHideDuration={10000}
+                        onClose={snackbarHandleClose}
+                        // action={snackbarAction}
+                    >
+                            <Alert onClose={snackbarHandleClose} severity="success" sx={{ width: '100%' }}>
+                                Email sent! We will get back to you as soon as possible.
+                            </Alert>
+                    </Snackbar>
+                    
                 </div>
                 }
             </div>
--- src/data/main_data.js
+++ src/data/main_data.js
@@ -5,7 +5,7 @@ const landingHero =
     {
         title: "Create your",
         text: 
-                `Create a full-featured Website Application fast and effortlessly. Take a look at some website projects created with Webframe in the /projects tab.
+                `Create a full-featured Website Application fast and effortlessly. Take a look at some website projects created with Webframe in the /solutions tab.
 
                 Wordpress, Webflow, Shopify, Squarespace or Wix are great for standard static website creation. But if you are looking to create your own professional Website Application without limitations and with the help of experts, try Webframe.`
     }
